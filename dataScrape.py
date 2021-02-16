4a1a902c8d68fecf9c29ecccf237da88222b2972
Abhiram824 <66290411+Abhiram824@users.noreply.github.com>
2021-02-16 16:21:34 -0600

    Modified git ignore, readme, and initialized constants for datascraping module

---
+++ dataScrape.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+PLAYERWEBSITE = "https://www.basketball-reference.com/players/"
+GENERALWEBSITE = "https://www.basketball-reference.com/"
+ALPHABET = ["a/", "b/", "c/", "d/", "e/", "f/", "g/", "h/", "i/", "j", "k/", "l/", "m/", "n/", "o/", "p/", "q/", "r/", "s/", "t/", "u/", "v/", "w/", "x/", "y/", "z/"]
+
