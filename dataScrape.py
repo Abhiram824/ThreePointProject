104e1539db19687b40d87899837f7a7317078eb9
Abhiram824 <66290411+Abhiram824@users.noreply.github.com>
2021-02-16 23:38:10 -0600

    added function to get desired data from a specified player

--- dataScrape.py
+++ dataScrape.py
@@ -7,6 +7,9 @@ ALPHABET = ["a/", "b/", "c/", "d/", "e/", "f/", "g/", "h/", "i/", "j", "k/", "l/
 FILTERATTRIBUTE = "data-stat"
 MINYEARPAIR = "year_min"
 PLAYERPAIR = "player"
+POSITIONPAIR = "pos"
+CAREERSTATSVAL = "Career"
+STATSPAIR = "season"
 
 
 def soupify(soupify_link):
@@ -15,26 +18,48 @@ def soupify(soupify_link):
     return webpage
 
 #takes in a webpage that has a bunch of links to players in a table
-#returns a list of player webpages who played after a specified year
-def filter_players_by_years(link, min_year):
-    player_link = ""
+#returns a list of player webpages who played after a specified year and play a certain position
+#will add more parameters later
+def filter(row, min_year, excludedPos):
+    add_to_list = True
+    for col in row.children: #while loop is more efficient, change later
+            #FILTERATTRIBUTE is an attribute in every column so do not need to check if it an attribute of the column
+        if(col[FILTERATTRIBUTE] == POSITIONPAIR):
+            position = col.string
+        elif(col[FILTERATTRIBUTE] == MINYEARPAIR):
+            player_start_year = int(col.string)
+    if (position in excludedPos or player_start_year < min_year):
+        add_to_list = False
+    return add_to_list
+
+
+def filter_players(link, min_year, position):
     filtered_years = []
     alphabet_webpage = soupify(link)
     table_body = alphabet_webpage.find("tbody")
     for row in table_body.find_all('tr'):
-        for col in row.children: #while loop is more efficient, change later
-            #FILTERATTRIBUTE is an attribute in every column so do not need to check if it an attribute of the column
-            if(col[FILTERATTRIBUTE] == PLAYERPAIR):
-                link  = col.find("a")["href"]
-            elif(col[FILTERATTRIBUTE] == MINYEARPAIR):
-                player_start_year = int(col.string)
-
-        if(player_start_year >= min_year):
-            filtered_years.append(link)
-
+        if(filter(row, min_year, position)):
+            filtered_years.append(row.find("a")["href"]) #hardcoded
     return filtered_years
 
-        
+
+
+#takes in a link to a page with the actual player's stats and a list of stats wanted 
+#returns the specified stats as the player's career avg
+def extract_stats(link, stats):
+    exact_stats = []
+    stats_page = soupify(link)
+    table_foot = stats_page.find("tfoot") #hardcoded make a generalized method to unhardcode this stuff
+    row_data = table_foot.find("tr")
+    for col in row_data:
+        if(col[FILTERATTRIBUTE] in stats):
+            exact_stats.append(float(col.string))
+
+    return exact_stats
+
+
+
+
 
 
         
