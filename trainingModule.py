f2297315296f69a1f90a24be218761af397e6a8f
Abhiram824 <66290411+Abhiram824@users.noreply.github.com>
2021-02-17 17:57:04 -0600

    added normalize and gradient functions to trainingModule.py

--- trainingModule.py
+++ trainingModule.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 # functions are for single variable linear regression
+def normalize(max, min, val):
+    return(val-min)/(max-min)
 
-def predict(listx, listy, weights, bias):
-    predictions = np.dot(listx, weights)
+def predict(listx, listy, weight, bias):
+    predictions = np.dot(listx, weight)
     predictions = (i + bias for i in predictions)
     return predictions
 
@@ -15,4 +17,22 @@ def cost(predictedVals, realVals):
     return cost/length
 
 
-#def gradient(listx, listy, weights, bias, learningRate):
+def gradient(listx, listy, weight, bias):
+    #calculating both bias and weight gradient to minimize iterations
+    weightGradient = 0
+    bGradient = 0
+    length = len(listy)
+    predictions = predict(listx, listy, weight, bias)
+    for i in range(length):
+        error = predictions[i] - listy[i]
+        weightGradient += 2 * error * listx[i]
+        bGradient += 2 * error
+    weightGradient /= length
+    bGradient /= length
+
+    return weightGradient, bGradient
+
+def updateVals(weight, bias, weightGradient, bGradient, learningRate):
+    weight += weightGradient * learningRate
+    bias += bGradient * learningRate
+    return weight, bias
