tested training functions and turns out I did gradient ascent instead of descent 0_0, so fixed that bug and added logic to display r^2. Final commit is soon

main.py
@@ -1,5 +1,8 @@
 import dataScrape as ds
 import trainingModule as tm
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 PLAYERWEBSITE = "https://www.basketball-reference.com/players/"
 GENERALWEBSITE = "https://www.basketball-reference.com/"
@@ -24,13 +27,21 @@ training_input_vals, training_output_vals, test_inputs, test_outputs = tm.divide
 weight = 0
 bias = 0
 
-weight, bias = tm.train(training_input_vals, training_output_vals, weight, bias, .05, 500)
+weight, bias = tm.train(training_input_vals, training_output_vals, weight, bias, .025, 500)
 predicted_vals = tm.predict(test_inputs, weight, bias)
 cost = tm.cost(predicted_vals, test_outputs)
 
 
-"""
-correlation_matrix = np.corrcoef(, y_values)
+correlation_matrix = np.corrcoef(predicted_vals, test_outputs)
 correlation_xy = correlation_matrix[0,1]
 r_squared = correlation_xy**2
-"""
+
+print(predicted_vals, test_outputs, weight, bias)
+print(r_squared, 1000 * cost)
+
+
+plt.plot(predicted_vals, predicted_vals)
+plt.scatter(test_inputs, test_outputs)
+
+plt.show()
+